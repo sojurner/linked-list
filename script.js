24f5298a04445ce87a2468f41e4cfb9c8eefe13c
Paul Kim <pykim0591@gmail.com>
2018-05-13 16:24:43 -0600

    UpdatedCSSHTMLJS

--- script.js
+++ script.js
@@ -1,63 +1,62 @@
 //Variables
-var  = document.queryselector('');
-var  = document.queryselector('');
-var  = document.queryselector('');
-var  = document.queryselector('');
-var  = document.queryselector('');
-
+var websiteTitle = document.querySelector('.input-website-title');
+var websiteUrl = document.querySelector('.website-url');
+var enterButton = document.querySelector('.enter-button');
+var displayReadCounter = document.querySelector('.read-counter');
+var dispalyNotReadCounter = document.querySelector('.not-read-counter');
+var displayTotalCounter = document.querySelector('.total-counter');
+var notreadCounter = document.querySelector('.not-read-counter');
+var bookmarkList = document.querySelector('.bookmark-content-list');
 //Functions
-function () {
 
-};
 
-function () {
-
-};
-
-function () {
-
-};
-
-function () {
-
+function bookmarkCreator() {
+  var bookmarkList = document.querySelector('.bookmark-content-list');
+  var newBookmark = document.createElement('article');
+  newBookmark.classList.add('bookmarks');  
+  newBookmark.innerHTMl =
+   `<h2>${websiteTitle.value}</h2>
+    <hr>
+    <a target='_blank' href="${websiteUrl.value}">${websiteUrl.value}</a>
+    <hr>
+      <button title = "Read Button" class = "read-button">Read</button>
+      <button title = "Delete Button" class = "delete-button">Delete</button>`
+  bookmarkList.prepend(newBookmark);
+  document.querySelector('form').reset();
 };
 
 
 //Events
 //---------
 //PHASE 1 
+enterButton.addEventListener('click', function() {
+  bookmarkCreator();
+  console.log(testing);
+});
 //---------
 //clicking "enter" button to create bookmark
-.addeventlistener() {
-};
+
 //clicking "delete" to remove bookmark
-.addeventlistener() {
-};
+
 //---------
 //PHASE 2 
 //---------
 //valid url creates bookmark
-.addeventlistener() {
-};
+
 //invalid url returns error 
-.addeventlistener() {
-};
+
 //---------
 //PHASE 3 
 //---------
 //disable/enable 
-.addeventlistener() {
-};
+
 //card count reflects read/unread/total 
 //clicking "read" button to increment 'read' counter
 //unclicked "read" button adds to 'unread' counter
 //'read' + 'unread'  = total counter 
-.addeventlistener() {
-};
+
 //--------
 //PHASE 4 
 //--------
 //clicking "clear" button to remove bookmarks clicked as "read"
-.addeventlistener() {
-};
 
