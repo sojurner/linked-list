b7760826f0747082275671cab147a8df9ec259c6
Paul Kim <pykim0591@gmail.com>
2018-05-13 10:01:14 -0600

    JSstructure HTMl&CSSupdate

---
+++ script.js
@@ -0,0 +1,63 @@
+//Variables
+var  = document.queryselector('');
+var  = document.queryselector('');
+var  = document.queryselector('');
+var  = document.queryselector('');
+var  = document.queryselector('');
+
+//Functions
+function () {
+
+};
+
+function () {
+
+};
+
+function () {
+
+};
+
+function () {
+
+};
+
+
+//Events
+//---------
+//PHASE 1 
+//---------
+//clicking "enter" button to create bookmark
+.addeventlistener() {
+};
+//clicking "delete" to remove bookmark
+.addeventlistener() {
+};
+//---------
+//PHASE 2 
+//---------
+//valid url creates bookmark
+.addeventlistener() {
+};
+//invalid url returns error 
+.addeventlistener() {
+};
+//---------
+//PHASE 3 
+//---------
+//disable/enable 
+.addeventlistener() {
+};
+//card count reflects read/unread/total 
+//clicking "read" button to increment 'read' counter
+//unclicked "read" button adds to 'unread' counter
+//'read' + 'unread'  = total counter 
+.addeventlistener() {
+};
+//--------
+//PHASE 4 
+//--------
+//clicking "clear" button to remove bookmarks clicked as "read"
+.addeventlistener() {
+};
+
