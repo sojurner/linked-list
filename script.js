ea6028d3ebe1ea2586af7bfe6ad31e7a43212cae
Paul Kim <pykim0591@gmail.com>
2018-05-15 08:29:31 -0600

    ReadCounterCSSModifications

--- script.js
+++ script.js
@@ -2,76 +2,45 @@
 var websiteTitle = document.querySelector('#website-title');
 var websiteUrl = document.querySelector('#website-url');
 var enterButton = document.querySelector('.enter-button');
-var displayReadCounter = document.querySelector('.read-counter');
-var displayNotReadCounter = document.querySelector('.not-read-counter');
-var displayTotalCounter = document.querySelector('.total-counter');
 var notReadCounter = document.querySelector('.not-read-counter');
 var titleUrlForm = document.querySelector('.title-url-form');
 var bookmarkList = document.querySelector('.bookmark-content-list');
-
+var displayReadCounter = document.querySelector('.read-counter');
+var displayNotReadCounter = document.querySelector('.not-read-counter');
+var displayTotalCounter = document.querySelector('.total-counter');
+var totalCounter = 0, readCounter = 0, notReadCounter = 0
 //Functions
 
-var disableEnterButton = function() {
-  if (websiteTitle === '' || websiteUrl === '') {
-    enterButton.disabled = true;
-  } else {
-    enterButton.disabled = false;
-  }
-};
+
+websiteTitle.addEventListener('input', disableEnterButton);
+
+websiteUrl.addEventListener('input', disableEnterButton);
+
+titleUrlForm.addEventListener('submit', bookmarkCreator); 
+
 
 function bookmarkCreator() {
+  event.preventDefault();
   var newBookmark = document.createElement('article');
-  newBookmark.classList.add('bookmark'); 
+  newBookmark.classList.add('bookmark');  
   newBookmark.innerHTML = 
   `<h2>${websiteTitle.value}</h2>
-   <hr>
-   <a target='_blank' href="${websiteUrl.value}">${websiteUrl.value}</a>
-   <hr>
-   <button title = "Read Button" class = "read-button">Read</button>
-   <button title = "Delete Button" class = "delete-button">Delete</button>`;
+  <hr>
+  <a target='_blank' href="${websiteUrl.value}">${websiteUrl.value}</a>
+  <hr>
+  <button title ="Read Button" class="read-button">Read</button>
+  <button title ="Delete Button" class ="delete-button">Delete</button>`;
   bookmarkList.prepend(newBookmark);
-  document.querySelector('form').reset();
+  titleUrlForm.reset();
+  totalCounter ++;
+  bookmarkCounter();
 };
 
-
-//Events
-//---------
-//PHASE 1 
-//---------
-//clicking "enter" button to create bookmark
-titleUrlForm.addEventListener('submit', function(e) {
-  e.preventDefault();
-});
-
-enterButton.addEventListener('click', function() {
-  bookmarkCreator();
-});
-//clicking "delete" to remove bookmark
-
-//---------
-//PHASE 2 
-//---------
-//valid url creates bookmark
-
-//invalid url returns error 
-
-//---------
-//PHASE 3 
-//---------
-//disable/enable 
-
-//card count reflects read/unread/total 
-
-//clicking "read" button to increment 'read' counter
-//unclicked "read" button adds to 'unread' counter
-//'read' + 'unread'  = total counter 
 function bookmarkCounter() {
-  cardCount = document.queryselector('.bookmark').length;
-  readCount = readBookmarks
-}
-
-//--------
-//PHASE 4 
-//--------
-//clicking "clear" button to remove bookmarks clicked as "read"
-
+  readCounter = document.querySelectorAll('.read').length
+  totalCounter = document.querySelectorAll('.bookmark').length;
+  notReadCounter = (totalCounter - readCounter);
+  displayReadCounter.innerText = readCounter;
+  displayTotalCounter.innerText = totalCounter;
+  displayNotReadCounter.innerText = notReadCounter;
+};
