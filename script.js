054421fb8f47bcfb86a35e16178a405d5eaff516
Thomas Scully <tpscully3@gmail.com>
2018-05-16 08:46:53 -0600

    Try to get read and delete button working

--- script.js
+++ script.js
@@ -1,14 +1,9 @@
 var websiteTitle = document.querySelector('#website-title');
 var websiteUrl = document.querySelector('#website-url');
 var enterButton = document.querySelector('.enter-button');
-var notReadCounter = document.querySelector('.not-read-counter');
 var titleUrlForm = document.querySelector('.title-url-form');
 var bookmarkList = document.querySelector('.bookmark-content-list');
-
-var displayReadCounter = document.querySelector('.read-counter');
-var displayNotReadCounter = document.querySelector('.not-read-counter');
-var displayTotalCounter = document.querySelector('.total-counter');
-var totalCounter = 0, readCounter = 0, notReadCounter = 0
+var readButton = document.querySelector('.read-button');
 
 var enableEnterButton = function() {
   if (websiteTitle !== '' && websiteUrl !== '') {
@@ -16,15 +11,26 @@ var enableEnterButton = function() {
   }
 };
 
-
-websiteUrl.addEventListener('keyup', enableEnterButton);
+websiteTitle.addEventListener('keyup', enableEnterButton);
 
 titleUrlForm.addEventListener('submit', bookmarkCreator); 
 
-function bookmarkCreator() {
+bookmarkList.addEventListener('click', handleBookmarkClick);
+
+function handleBookmarkClick(event) {
+  if (event.target.className === 'read-button') {
+    newBookmark.classList.toggle('read');
+    readCounter ++;
+  }
+  if (event.target.className === 'delete-button') {
+    newBookmark.remove();
+  }  
+};
+
+function bookmarkCreator(event) {
   event.preventDefault();
   var newBookmark = document.createElement('article');
-  newBookmark.classList.add('bookmark');  
+  newBookmark.classList = 'bookmark';  
   newBookmark.innerHTML = 
   `<h2>${websiteTitle.value}</h2>
   <hr>
@@ -34,17 +40,17 @@ function bookmarkCreator() {
   <button title ="Delete Button" class ="delete-button">Delete</button>`;
   bookmarkList.prepend(newBookmark);
   titleUrlForm.reset();
-  totalCounter ++;
   bookmarkCounter();
 };
 
 function bookmarkCounter() {
-  readCounter = document.querySelectorAll('.read').length
-  totalCounter = document.querySelectorAll('.bookmark').length;
-  notReadCounter = (totalCounter - readCounter);
-  displayReadCounter.innerText = readCounter;
-  displayTotalCounter.innerText = totalCounter;
-  displayNotReadCounter.innerText = notReadCounter;
+  var readCounter = document.querySelector('.read-counter');
+  var totalCounter = document.querySelector('.total-counter');
+  var notReadCounter = document.querySelector('.not-read-counter');
+  readCount = document.querySelectorAll('.read').length;
+  totalCount = document.querySelectorAll('.bookmark').length;
+  notReadCount = (totalCount - readCount);
+  readCounter.innerText = readCount;
+  totalCounter.innerText = totalCount;
+  notReadCounter.innerText = notReadCount;
 };
- 
-
